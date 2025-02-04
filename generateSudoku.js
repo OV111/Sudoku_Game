aa5beda52f52b54db6d2f634f3534bc30bca7865
Vahe <ohanyanv703@gmail.com>
2025-02-05 00:15:10 +0400

    Generate Sudoku Puzzle!

--- generateSudoku.js
+++ generateSudoku.js
@@ -1,81 +1,73 @@
 export function initializeGrid() {
-   let grid = [];
-   for(let i = 0;i < 9; ++i) {
-        let row = Array(9).fill(0);
+    let grid = [];
+    for(let i = 0;i < 9; ++i) {
+        let row = Array(9).fill(0);  // null
         grid.push(row);
     }
-    fillGrid(grid);
-    
+    generatePuzzle(grid);    // Generating puzzle first 
+    fillGrid(grid);         // Then filling it! 
 }
 function fillGrid(grid) {
     let squares = document.querySelectorAll(".square p");
     for(let r = 0; r < 9; ++r) {
         for(let c = 0; c < 9; ++c) {
-            // let index = r * 9 + c;
+            let index =  r * 9 + c;
             squares[r * 9 + c].textContent = grid[r][c];
         }
     }
-    // console.log(grid)
-    generatePuzzle(grid)
 }
 
-initializeGrid()
 
 function generatePuzzle(grid) {   // main function (generating valid puzzle)
-    backtrack(grid)
+    backtrack(grid);
 
 }
 
+function removeNumbers(grid) {
+
+}
 
 function backtrack(grid) {   
-        for(let r = 0; r < 9; ++r) {
-            for(let c = 0; c < 9; ++c) {
-                if(grid[r][c] == 0) {
-                    
-                    for(let num = 1; num <= 9; ++num) {
-                        if(isValid(grid,r,c,num)) {
-                            grid[r][c] = num;
-                            if(backtrack(grid)) {
-                                return true;
-                            } else {
-                                grid[r][c] = 0;
-                            }
-                        }
+    for(let r = 0; r < 9; ++r) {      //Looping through row & columns
+        for(let c = 0; c < 9; ++c) {
+            if(grid[r][c] === 0) {                    // checking if cell is free(=== 0)
+                for(let num = 1; num <= 9; ++num) {  // Looping for trying putting numbers  
+                    if(isValid(grid,r,c,num)) {     // if Valid number put the number
+                        grid[r][c] = num;
+                        if(backtrack(grid)) { return true; }  //// !Recurive call
+                        grid[r][c] = 0;
                     }
-                    return false;
                 }
-            }
+                return false;
+            } 
         }
-        return true;
+   }
+   return true;
 }
 
-function isValid(grid,row,col,num) {      // Validation of numbers
-    for(let index = 0; index < 9; ++index) {
-        if(grid[row][index] === num) {
-            return false;
+function isValid(grid,row,col,num) {      // Validation of numbers (cells)
+    for(let i = 0; i < 9; ++i) {
+        if(grid[row][i] === num) {   // checking if number of cell(in row),
+            return false;           // isn't repeating by horizontal
         }
     }
-    for(let index = 0; index < 9; ++index) {
-        if(grid[index][col] === num) {
-            return false;
+    for(let i = 0; i < 9; ++i) {
+        if(grid[i][col] === num) {  // checking if number of cell(in column) 
+            return false;          // isn't repeating by vertically
         }
     }
 
-    let boxRow = row - row % 3;
-    let boxCol = col - col % 3;
-    
-    for(let i = 0; i < 3; ++i) {
+    // Box 3x3 Sub-Grid
+    let BoxStartRow = Math.floor(row / 3) * 3;  // Defining the start of box
+    let BoxStartColumn = Math.floor(col / 3) * 3;
+    for(let i = 0; i < 3; ++i){         // checking for specific box all cells
         for(let j = 0; j < 3; ++j) {
-            if(grid[boxRow + i][boxCol + j] === num) {
+            if(grid[BoxStartRow + i][BoxStartColumn + j] === num) {  //!Chechking Each Cell
                 return false;
             }
         }
     }
     return true;
-
-
-    // Box3x3
-    
 }
 
 
@@ -89,74 +81,15 @@ function isValid(grid,row,col,num) {      // Validation of numbers
 
 
 
-// Function to generate a valid Sudoku puzzle
-// function generatePuzzle(grid) {
-//     // Step 1: Fill the grid using backtracking
-//     backtrack(grid);
-    
-//     // Step 2: Randomly remove numbers to create a puzzle
-//     let cellsToRemove = 40;  // You can adjust this for puzzle difficulty
-//     while (cellsToRemove > 0) {
-//         let row = Math.floor(Math.random() * 9);
-//         let col = Math.floor(Math.random() * 9);
-        
-//         // Remove the number if it's not already 0
-//         if (grid[row][col] !== 0) {
-//             grid[row][col] = 0;
-//             cellsToRemove--;
-//         }
-//     }
-// }
 
-// // Backtracking function to fill the grid
-// function backtrack(grid) {
-//     for (let r = 0; r < 9; r++) {
-//         for (let c = 0; c < 9; c++) {
-//             if (grid[r][c] === 0) {  // Find an empty cell
-//                 // Try numbers 1-9
-//                 for (let num = 1; num <= 9; num++) {
-//                     if (isValid(grid, r, c, num)) {
-//                         grid[r][c] = num;  // Place the number
-//                         if (backtrack(grid)) {  // Recursively try to fill the next cells
-//                             return true;  // If successful, return true
-//                         }
-//                         grid[r][c] = 0;  // Backtrack if no valid number found
-//                     }
-//                 }
-//                 return false;  // No valid number can be placed, backtrack
-//             }
-//         }
-//     }
-//     return true;  // All cells are filled correctly
-// }
-
-// // Function to check if placing a number is valid
-// function isValid(grid, row, col, num) {
-//     // Check the row for duplicates
-//     for (let i = 0; i < 9; i++) {
-//         if (grid[row][i] === num) {
-//             return false;
-//         }
-//     }
-    
-//     // Check the column for duplicates
-//     for (let i = 0; i < 9; i++) {
-//         if (grid[i][col] === num) {
-//             return false;
-//         }
-//     }
-    
-//     // Check the 3x3 subgrid for duplicates
-//     let boxRowStart = Math.floor(row / 3) * 3;
-//     let boxColStart = Math.floor(col / 3) * 3;
-    
-//     for (let r = boxRowStart; r < boxRowStart + 3; r++) {
-//         for (let c = boxColStart; c < boxColStart + 3; c++) {
-//             if (grid[r][c] === num) {
-//                 return false;
-//             }
-//         }
-//     }
-    
-//     return true;  // The number is valid
-// }
+// [0,0] [0,1] [0,2] | [0,3] [0,4] [0,5] | [0,6] [0,7] [0,8]
+// [1,0] [1,1] [1,2] | [1,3] [1,4] [1,5] | [1,6] [1,7] [1,8]
+// [2,0] [2,1] [2,2] | [2,3] [2,4] [2,5] | [2,6] [2,7] [2,8]
+// -------------------|-------------------|-------------------
+// [3,0] [3,1] [3,2] | [3,3] [3,4] [3,5] | [3,6] [3,7] [3,8]
+// [4,0] [4,1] [4,2] | [4,3] [4,4] [4,5] | [4,6] [4,7] [4,8]
+// [5,0] [5,1] [5,2] | [5,3] [5,4] [5,5] | [5,6] [5,7] [5,8]
+// -------------------|-------------------|-------------------
+// [6,0] [6,1] [6,2] | [6,3] [6,4] [6,5] | [6,6] [6,7] [6,8]
+// [7,0] [7,1] [7,2] | [7,3] [7,4] [7,5] | [7,6] [7,7] [7,8]
+// [8,0] [8,1] [8,2] | [8,3] [8,4] [8,5] | [8,6] [8,7] [8,8]
